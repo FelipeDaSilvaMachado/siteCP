41d482e643d9244aa65a5aae07dece22bf489f2d
FelipeDaSilvaMachado <felipe.silvamachado@yahoo.com>
2025-08-10 22:00:30 -0300

    subindo a revisao de html css

---
+++ resources/views/site.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link href="{{ asset('assets/css/site.css') }}" rel="stylesheet">
+    <title>Campeonato Paulista 2025</title>
+</head>
+
+<body>
+    <div class='header'>
+        <h1>Paulistão 2025!</h1>
+        <div class='fpf'>
+            <a href="https://www.futebolpaulista.com.br/Home/"><img src="{{asset ('assets/img/fpfLogo.png')}}" alt="Federação Paulista"></a>
+        </div>
+    </div>
+    <div class='h3'>
+        <h3>
+            A serie A1, principal divisao do campeonato paulista, conta em sua primeira fase com 16 times iniciais.
+            A primeira fase é formada por 4 grupos de 4 times e os times enfrentam os adversários que não estão no seu grupo.
+            Ao passar de fase o primeiro de cada grupo enfrenta o segundo do mesmo grupo, com jogos de ida com o melhor do grupo sendo o mandante dos jogos até a semi-final.
+            Na final, serão jogos de ida e volta com o melhor time do campeonato mandando o último jogo em casa.
+        </h3>
+    </div>
+    <div class='ingresso'>
+        <p>
+            Clique no seu time para adquirir seu ingresso.
+        </p>
+    </div>
+    <div class="clubes">
+        <div>
+            <tr>
+                <td>
+                    <a href="https://www.palmeiras.com.br/"><img src="{{ asset('assets/img/palmeiras.png') }}" alt="Palmeiras"></a>
+                </td>
+                <td>
+                    <a href="https://www.santosfc.com.br/"><img src="{{ asset('assets/img/santos.png') }}" alt="Santos"></a>
+                </td>
+                <td>
+                    <a href="https://www.corinthians.com.br/"><img src="{{ asset('assets/img/corinthians.png') }}" alt="Corinthians"></a>
+                </td>
+                <td>
+                    <a href="https://www.santosfc.com.br/"><img src="{{ asset('assets/img/sao-paulo logo.png') }}" alt="São Paulo"></a>
+                </td>
+            </tr>
+        </div>
+        <div>
+            <tr>
+                <td>
+                    <a href="https://www.redbullbragantino.com.br/"><img src="{{ asset('assets/img/red-bull-bragantino logo.png') }}" alt="Bragantino"></a>
+                </td>
+                <td>
+                    <a href="https://www.mirassolfc.com.br/"><img src="{{ asset('assets/img/Mirassol_FC_logo.png') }}" alt="Mirassol"></a>
+                </td>
+                <td>
+                    <a href="https://pontepreta.com.br/"><img src="{{ asset('assets/img/ponte preta.png') }}" alt="Ponte Preta"></a>
+                </td>
+                <td>
+                    <a href="http://ecsaobernardo.com.br/"><img src="{{ asset('assets/img/sao bernardo.png') }}" alt="São Bernardo"></a>
+                </td>
+            </tr>
+        </div>
+        <div>
+            <tr>
+                <td>
+                    <a href="https://botafogofutebolsa.com.br/"><img src="{{ asset('assets/img/botafogo sp.png') }}" alt="Botafogo - SP"></a>
+                </td>
+                <td>
+                    <a href="https://www.ecsantoandre.com.br/"><img src="{{ asset('assets/img/santo-andre.png') }}" alt="Santo André"></a>
+                </td>
+                <td>
+                    <a href="https://interdelimeira.com/"><img src="{{ asset('assets/img/interLimeira.png') }}" alt="Inter de Limeira"></a>
+                </td>
+                <td>
+                    <a href="https://ferroviariasaf.com/"><img src="{{ asset('assets/img/ferroviaria_escudo.png') }}" alt="Ferroviária"></a>
+                </td>
+            </tr>
+        </div>
+        <div>
+            <tr>
+                <td>
+                    <a href="https://ituanofc.com/"><img src="{{ asset('assets/img/ituano.png') }}" alt="Ituano"></a>
+                </td>
+                <td>
+                    <a href="https://www.ecaguasanta.com/"><img src="{{ asset('assets/img/aguaSanta.png') }}" alt="Água Santa"></a>
+                </td>
+                <td>
+                    <a href="https://guaranifc.com.br/"><img src="{{ asset('assets/img/guarani-logo.png') }}" alt="Guarani"></a>
+                </td>
+                <td>
+                    <a href="https://www.gremionovorizontino.com.br/"><img src="{{ asset('assets/img/gremioNovorizontino.png') }}" alt="Novorizontino"></a>
+                </td>
+            </tr>
+        </div>
+    </div>
+    <div class='rodape'>
+        <footer>
+            <p>
+                ©Todos os direitos reservados.
+            </p>
+        </footer>
+    </div>
+</body>
+
+</html>
